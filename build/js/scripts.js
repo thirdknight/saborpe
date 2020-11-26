f2354d29acdd3d8a8e1d9fdbe50246aa47cc407e
Edinson <edinson.ningles@gmail.com>
2020-11-26 16:52:32 +0100

    Add swiper slider

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,4 +1,4 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}({"./src/js/base/variables.js":
+!function(e){var t={};function r(n){if(t[n])return t[n].exports;var s=t[n]={i:n,l:!1,exports:{}};return e[n].call(s.exports,s,s.exports,r),s.l=!0,s.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var s in e)r.d(n,s,function(t){return e[t]}.bind(null,s));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}({"./src/js/base/variables.js":
 /*!**********************************!*\
   !*** ./src/js/base/variables.js ***!
   \**********************************/
@@ -6,13 +6,17 @@
 /*!***********************************!*\
   !*** ./src/js/components/test.js ***!
   \***********************************/
-/*! exports provided: default */function(e,t,n){"use strict";n.r(t);t.default=class{constructor(e,t){this.name=e,this.age=t}getInfo(){return`${this.name} and he is ${this.age} years old`}}},"./src/js/scripts.js":
+/*! exports provided: default */function(e,t,r){"use strict";r.r(t);t.default=class{constructor(e,t){this.name=e,this.age=t}getInfo(){return`${this.name} and he is ${this.age} years old`}}},"./src/js/scripts.js":
 /*!***************************!*\
   !*** ./src/js/scripts.js ***!
   \***************************/
-/*! no exports provided */function(e,t,n){"use strict";n.r(t);const r=new(n(/*! ./components/test */"./src/js/components/test.js").default)("Edinson is Software Enginure from Microsof",40);console.log(r.getInfo())},0:
-/*!****************************************************************************************!*\
-  !*** multi ./src/js/scripts.js ./src/js/base/variables.js ./src/js/components/test.js ***!
-  \****************************************************************************************/
-/*! no static exports found */function(e,t,n){n(/*! /Users/edinson/Sites/saborpe/src/js/scripts.js */"./src/js/scripts.js"),n(/*! /Users/edinson/Sites/saborpe/src/js/base/variables.js */"./src/js/base/variables.js"),e.exports=n(/*! /Users/edinson/Sites/saborpe/src/js/components/test.js */"./src/js/components/test.js")}});
+/*! no exports provided */function(e,t,r){"use strict";r.r(t);const n=new(r(/*! ./components/test */"./src/js/components/test.js").default)("Edinson is Software Enginure from Microsof",40);console.log(n.getInfo())},"./src/js/slider.js":
+/*!**************************!*\
+  !*** ./src/js/slider.js ***!
+  \**************************/
+/*! no static exports found */function(e,t){new Swiper(".swiper-container",{effect:"coverflow",grabCursor:!0,centeredSlides:!0,slidesPerView:"auto",coverflowEffect:{rotate:50,stretch:0,depth:200,modifier:1,slideShadows:!0},loop:!0})},0:
+/*!***********************************************************************************************************!*\
+  !*** multi ./src/js/scripts.js ./src/js/slider.js ./src/js/base/variables.js ./src/js/components/test.js ***!
+  \***********************************************************************************************************/
+/*! no static exports found */function(e,t,r){r(/*! /Users/edinson/Sites/saborpe/src/js/scripts.js */"./src/js/scripts.js"),r(/*! /Users/edinson/Sites/saborpe/src/js/slider.js */"./src/js/slider.js"),r(/*! /Users/edinson/Sites/saborpe/src/js/base/variables.js */"./src/js/base/variables.js"),e.exports=r(/*! /Users/edinson/Sites/saborpe/src/js/components/test.js */"./src/js/components/test.js")}});
 //# sourceMappingURL=../sourcemaps/scripts.js.map
--- index.php
+++ index.php
@@ -11,6 +11,7 @@
   <meta property="og:url" content="">
   <meta property="og:image" content="">
   <link rel="apple-touch-icon" href="icon.png">
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <link href="build/css/styles.css" rel="stylesheet">
   <!-- <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon"> -->
   <title>Saborpe</title>
@@ -22,6 +23,21 @@
     <main>
       <?php include("includes/section-banner.php");?>
       <?php include("includes/section-description.php");?>
+      <div class="section">
+        <div class="container">
+        <div class="swiper-container">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="build/assets/images/plate1.jpg"></div>
+            <div class="swiper-slide"><img src="build/assets/images/plate2.jpg"></div>
+            <div class="swiper-slide"><img src="build/assets/images/plate3.jpg"></div>
+            <div class="swiper-slide"><img src="build/assets/images/plate4.jpg"></div>
+            <div class="swiper-slide"><img src="build/assets/images/plate1.jpg"></div>
+            <div class="swiper-slide"><img src="build/assets/images/plate2.jpg"></div>
+          </div>
+      </div>
+        </div>
+      </div>
+      
     </main>
     <?php include("includes/footer.php");?>
     </div>
@@ -31,5 +47,6 @@
     window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
   </script>
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
   <script src="build/js/scripts.js"></script>
 </html>
